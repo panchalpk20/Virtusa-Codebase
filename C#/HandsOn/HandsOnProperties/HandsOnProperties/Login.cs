b59fff35b233dee87181cf4adbe6094edbc9b667
sandotnet <87011674+sandotnet@users.noreply.github.com>
2022-07-21 18:44:52 +0530

    new_commit

---
+++ C#/HandsOn/HandsOnProperties/HandsOnProperties/Login.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HandsOnProperties
+{
+    class Login
+    {
+        //Automatic Properties
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; } = "Admin"; //set default value to the Property
+    }
+}
